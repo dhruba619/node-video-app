278683cae9d6d35374864183d6476a6504a3a4ff
Piyali Nath <p.nath@carepay.com>
2020-08-29 23:56:25 +0200

    second phase

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
-myVideo.muted = true;
+myVideo.muted = false;
 
 var peer = new Peer(undefined, {
     path: '/peerjs',
@@ -12,7 +12,7 @@ var peer = new Peer(undefined, {
 let myVideoStream;
 navigator.mediaDevices.getUserMedia({ 
     video: true,
-    audio: false
+    audio: true
 }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, myVideoStream);
@@ -27,6 +27,20 @@ navigator.mediaDevices.getUserMedia({
 
     socket.on('user-connected', (userId) => {
         connectToNewUser(userId, stream);
+    });
+
+    let text = $('input');
+    $('html').keydown((e) => {
+        if(e.which == 13 && text.val().length!= 0) {
+            console.log(text.val());
+            socket.emit('message',text.val());
+            text.val('');
+        }
+    });
+
+    socket.on('createMessage',  message => {
+        $('ul').append(`<li class="message"><b>user</b><br/>${message}</li>`);
+        scrollToBottom();
     });
 });
 
@@ -51,4 +65,34 @@ const addVideoStream = (video, stream) => {
         video.play();
     });
     videoGrid.append(video);
+}
+
+const scrollToBottom = () => {
+    let d = $('.main__chat__window');
+    d.scrollTop(d.prop("scrollHeight"));
+}
+
+const muteUnmute = () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    if(enabled) {
+        myVideoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
+    } else {
+        myVideoStream.getAudioTracks()[0].enabled = true;
+        setMuteButton();
+    }
+}
+
+const setMuteButton = () => {
+    const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>`
+    document.querySelector('.main__mute__button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+    const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>`
+    document.querySelector('.main__mute__button').innerHTML = html;
 }
