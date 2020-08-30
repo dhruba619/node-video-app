09bb712e76319d64df4ff129426565aba1696798
Piyali Nath <p.nath@carepay.com>
2020-08-30 12:02:36 +0200

    Video stop start

--- public/script.js
+++ public/script.js
@@ -96,3 +96,28 @@ const setUnmuteButton = () => {
     <span>Unmute</span>`
     document.querySelector('.main__mute__button').innerHTML = html;
 }
+
+const playStop = () => {
+    let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    if(enabled) {
+        myVideoStream.getVideoTracks()[0].enabled = false;
+        setPlayVideo();
+    } else {
+        setStopVideo();
+        myVideoStream.getVideoTracks()[0].enabled = true;
+    }
+}
+
+const setPlayVideo = () => {
+    const html = `
+    <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>`
+    document.querySelector('.main__video__button').innerHTML = html;
+}
+
+const setStopVideo = () => {
+    const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>`
+    document.querySelector('.main__video__button').innerHTML = html;
+}
